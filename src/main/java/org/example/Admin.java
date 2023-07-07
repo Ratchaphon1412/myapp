4f5c7364804eb74fef4d4afebc945fe6a9030661
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-07 14:49:20 +0700

    add constructer admin class

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,15 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Admin {
     private String name;
     private String password;
     private String role;
+    private ArrayList<String> listUser;
 
     public Admin(String name_admin , String password_admin,String role){
         this.name = name_admin;
         this.password = password_admin;
         this.role = role;
+        this.listUser = new ArrayList<String>();
     }
+    public Admin(String name_admin,String password_admin,String role,ArrayList<String>list_user){
+        this.name = name_admin;
+        this.password = password_admin;
+        this.role = role;
+        this.listUser = list_user;
+
+    }
+
+
+
+
 
     public void setName(String name) {
         this.name = name;
