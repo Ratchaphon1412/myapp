ec82d5072c4aff855c418c62215e2bc21d002f9c
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-07-07 14:52:42 +0700

    create config

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Admin {
     private String name;
     private String password;
     private String role;
 
+    private ArrayList<String> userList;
+
     public Admin(String name_admin , String password_admin,String role){
         this.name = name_admin;
         this.password = password_admin;
         this.role = role;
+    }
+    public Admin(String name_admin ,String password_admin,String role, ArrayList<String> user_list){
+        this.name = name_admin;
+        this.password = password_admin;
+        this.role = role;
+        this.userList = user_list;
     }
 
     public void setName(String name) {
